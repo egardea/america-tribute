9610889e29950e827d72e6478842ffc4f391b2ed
egardea <erik1gardea@gmail.com>
2018-12-18 13:17:35 -0600

    None

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-$(document).ready(function(){
-    $('.next').on('click', function(){
-        var currentImg = $('.active');
-        var nextImg = currentImg.next();
-
-        if(nextImg.lenght){
-            currentImg.removeClass('active').css('display', none);
-            nextImg.addClass('active').css('display', inline-block);
-        }
-    });
-
-    $('.prev').on('click', function(){
-        var currentImg = $('.active');
-        var prevImg = currentImg.prev();
-
-        if(prevImg.lenght){
-            currentImg.removeClass('active').css('display', none);
-            prevImg.addClass('active').css('display', inline-block);
-        }
-    });
-});
+function slider(){
+    document.getElementsByClassName('next').addEventListener('click', function(){
+        var currentImg = document.getElementsByClassName('active')
+        
+    })
+}
