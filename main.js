d3a8549cb1b1ac620e33bfb35e0bc2b704a3a061
egardea <erik1gardea@gmail.com>
2018-12-19 13:20:56 -0600

    image slider now fully finished

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
-function slider(){
-    document.getElementsByClassName('next').addEventListener('click', function(){
-        var currentImg = document.getElementsByClassName('active')
-        
-    })
-}
+let slideImg = document.querySelectorAll('.inner-slider img'),
+    arrowLeft = document.querySelector('.prev'),
+    arrowRight = document.querySelector('.next'),
+    current = 0;
+//Clear all Images
+    function reset(){
+        for(let i = 0; i < slideImg.length; i++){
+            slideImg[i].style.display = 'none';
+        }
+    }
+
+    //Initializes the starter
+    function startSlide(){
+        reset();
+        slideImg[0].style.display = 'inline-block';
+    }
+
+    //Show previous 
+    function slideLeft(){
+        reset();
+        slideImg[current - 1].style.display = 'inline-block';
+        current--;
+    }
+
+    //Show Next
+    function slideRight(){
+        reset();
+        slideImg[current + 1].style.display = 'inline-block';
+        current++;
+    }
+
+    //Left arrow click
+    arrowLeft.addEventListener('click', function(){
+        if(current === 0){
+            current = slideImg.length;
+        }
+        slideLeft();
+    });
+
+    //Right arrow Click
+    arrowRight.addEventListener('click', function(){
+        if(current === slideImg.length - 1){
+            current = -1
+        }
+        slideRight();
+    });
+    
+    startSlide();
